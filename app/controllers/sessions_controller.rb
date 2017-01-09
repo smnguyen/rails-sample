0f6c3c3162a97dd51e2748dff29786c145dbabbf
Sammy Nguyen <nguyesam@gmail.com>
2017-01-09 19:56:48 +0000

    Finish 10.2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(login_info[:password])
       log_in @user
       login_info[:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_to @user
+      redirect_back_or @user
     else
       flash.now[:danger] = 'Invalid email/password combination'  # Not quite right!
       render 'new'
