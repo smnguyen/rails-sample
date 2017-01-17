9366af3325c159a422fb31ca7e983f4a4080c29c
Sammy Nguyen <nguyesam@gmail.com>
2017-01-17 19:41:07 +0000

    Follow/unfollow users in app

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -13,6 +13,9 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_select 'title', full_title(@user.name)
     assert_select 'h1', text: @user.name
     assert_select 'h1 > img.gravatar'
+    # TODO https://www.railstutorial.org/book/following_users#sec-exercises_stats_and_a_follow_form
+    # assert_select 'strong#following', text: '49'
+    # assert_select 'strong#followers', text: '38'
     assert_match @user.microposts.count.to_s, response.body
     assert_select 'div.pagination', count: 1
     @user.microposts.paginate(page: 1).each do |micropost|
